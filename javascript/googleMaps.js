0515728b1584822dc258303445774845f023b143
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-11-21 04:40:57 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/googlemap/trunk@5419 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- javascript/googleMaps.js
+++ javascript/googleMaps.js
@@ -1640,16 +1640,6 @@ function mapFunctionIsDefined(variable) {
 }
 
 
-function strpos( haystack, needle, offset){
-	return false;
-	// http://kevin.vanzonneveld.net
-	// +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-	// *     example 1: strpos('Kevin van Zonneveld', 'e', 5);
-	// *     returns 1: 14
-	var i = haystack.indexOf( needle, offset ); // returns -1
-	return i >= 0 ? i : false;
-}
-
 
 
 
